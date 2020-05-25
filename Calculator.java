b33d51797552ec298611bd57471926c585b1f610
Tharoon T Thiagarajan <tharoon.raj@gmail.com>
2020-05-25 16:23:02 -0500

    Added Subtract

--- Calculator.java
+++ Calculator.java
@@ -3,8 +3,13 @@ class Calculator{
     int c = a + b;
     System.out.println(c);
   }
+  public void subtract(int a, int b){
+    int c = a - b;
+    System.out.println(c);
+  }
   public static void main(String args[]){
     Calculator obj = new Calculator();
     obj.add(2,3);
+    obj.subtract(10,5);
   }
 }
