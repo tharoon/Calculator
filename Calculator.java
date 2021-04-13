ff7d7fa7043274ce27bd24be17a334d9bd6ff0b4
Tharoon T Thiagarajan <tharoon.raj@gmail.com>
2021-04-13 16:32:06 -0400

    Added message method

--- Calculator.java
+++ Calculator.java
@@ -15,11 +15,17 @@ class Calculator{
     int c = a / b;
     System.out.println(c);
   }
+  
+  public String message(){
+    return "Vanakam Ulagam!";
+  }
+  
   public static void main(String args[]){
     Calculator obj = new Calculator();
     obj.add(2,3);
     obj.subtract(10,5);
     obj.multiply(2,8);
     obj.divide(100,5);
+    System.out.println(obj.message);
   }
 }
